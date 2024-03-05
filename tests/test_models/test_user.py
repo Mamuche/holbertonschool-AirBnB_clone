9f671ce31e7b2d1ab9a2f73c4cb6be84e34a665b
Mamuche <marionlaroche17@gmail.com>
2024-03-05 15:48:52 +0100

    add test-user file

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Test unittest for class User"""
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """blablabla"""
+
+    def test_args(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
