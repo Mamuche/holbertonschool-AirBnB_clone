4e607a4632f13b12c4a9378516c402787601caf1
Mamuche <marionlaroche17@gmail.com>
2024-03-05 14:24:46 +0100

    task7

--- console.py
+++ console.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """program contains the entry point of the command interpreter"""
 import cmd
+from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """class for command interpreter"""
     prompt = "(hbnb) "
+    __valid_classes = {
+        "BaseModel": BaseModel
+    }
 
     def do_quit(self, arg):
         """cmd to exit the program"""
@@ -19,6 +24,99 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """cmd shouldn't execute anything"""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id. Ex: $ create BaseModel"""
+        if not arg:
+            print("** class name missing **")
+        elif arg in self.__valid_classes:
+            new_instance = self.__valid_classes[arg]()
+            models.storage.save()
+            print(new_instance.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of
+        an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        elif args[0] not in self.__valid_classes:
+            print("** class doesn't exist **")
+            return
+        elif len(args) < 2 or not args[1]:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1] if len(args) > 1 else "")
+        if key not in models.storage.all():
+            print("** no instance found **")
+        else:
+            print(models.storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in self.__valid_classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            all_objects = models.storage.all()
+            obj = all_objects.get(key)
+            if obj:
+                del all_objects[key]
+                models.storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all
+        instances based or not on the class name"""
+        args = arg.split()
+        all_instances = []
+
+        if not args:
+            for obj in models.storage.all().values():
+                all_instances.append(str(obj))
+            print(all_instances)
+        elif args[0] not in self.__valid_classes:
+            print("** class doesn't exist **")
+        else:
+            for key, obj in models.storage.all().items():
+                if args[0] in key:
+                    all_instances.append(str(obj))
+            print(all_instances)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name
+        and id by adding or updating attribute"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in self.__valid_classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            all_objects = models.storage.all()
+            obj = all_objects.get(key)
+            if obj:
+                setattr(obj, args[2], eval(args[3]))
+                models.storage.save()
+            else:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
