9bbd04b597cd7f5cdda2a911d8ebc56dfa4001bc
Mamuche <marionlaroche17@gmail.com>
2024-03-05 11:51:31 +0100

    add file test

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Test unittest for class FileStorage"""
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """blablabla"""
+
+    def test_(self):
+
+class TestMethodSave(unittest.TestCase):
+    """blablabla"""
+
+
+class TestMethodAll(unittest.TestCase):
+    """blablabla"""
+
+
+class TestMethodNew(unittest.TestCase):
+    """blablabla"""
+
+class TestMethodReload(unittest.TestCase):
+    """blablabla"""
+
+
+if __name__ == '__main__':
+    unittest.main()
