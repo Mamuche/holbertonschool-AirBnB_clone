ddde8a15f6c84c246a5b2758fd6abb09f037d777
Mamuche <marionlaroche17@gmail.com>
2024-03-05 09:59:04 +0100

    restart project

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""create class BaseModel"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """class for all other classes to inherit from"""
+    def __init__(self, *args, **kwargs):
+        """constructor"""
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k in ['created_at', 'updated_at']:
+                        if isinstance(v, str):
+                            """convert strings into datetime object
+                            if it's not already object"""
+                            v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """print : name of class, id, dict"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute
+        updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return (new_dict)
